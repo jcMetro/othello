3140ccd0c9b9b509bd1447b00d7e83dbf299abbc
johnnychow <johnnykinchow@gmail.com>
2018-02-13 22:59:19 +0800

    initial othello state

--- src/main/java/org/jcMetro/Othello.java
+++ src/main/java/org/jcMetro/Othello.java
@@ -2,5 +2,34 @@ package org.jcMetro;
 
 public class Othello {
 
+    private final char[][] cells ;
 
+    public Othello() {
+        this.cells = new char[8][8];
+
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                this.cells[i][j] = '-';
+            }
+        }
+
+        this.cells[3][3] = 'O';
+        this.cells[3][4] = 'X';
+        this.cells[4][3] = 'X';
+        this.cells[4][4] = 'O';
+    }
+
+    public String displayBoard() {
+
+        String result = "";
+        for (int i = 0; i < 8; i++) {
+            result += (i+1);
+            result += " ";
+            result += String.valueOf(this.cells[i]);
+            result += '\n';
+        }
+
+        result += "  abcdefgh";
+        return result;
+    }
 }
--- src/test/java/org/jcMetro/OthelloTest.java
+++ src/test/java/org/jcMetro/OthelloTest.java
@@ -2,12 +2,28 @@ package org.jcMetro;
 
 import org.junit.Test;
 
+import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
 
 public class OthelloTest {
 
     @Test
-    public void firstCase() {
+    public void initial_state() {
+        Othello othello = new Othello();
 
+        assertThat(othello.displayBoard(), is(initialState()));
+    }
+
+    private String initialState() {
+        return String.join("\n",
+                "1 --------",
+                "2 --------",
+                "3 --------",
+                "4 ---OX---",
+                "5 ---XO---",
+                "6 --------",
+                "7 --------",
+                "8 --------",
+                "  abcdefgh");
     }
 }
