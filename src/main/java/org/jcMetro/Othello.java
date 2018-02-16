e7263cd9116efaf5489b4af3f7729af2cd96efac
johnnychow <johnnykinchow@gmail.com>
2018-02-16 10:27:41 +0800

    first step logic, need to implement all direction and refactoring

--- src/main/java/org/jcMetro/Othello.java
+++ src/main/java/org/jcMetro/Othello.java
@@ -1,12 +1,48 @@
 package org.jcMetro;
 
-import java.awt.*;
-import java.util.Arrays;
+import java.util.*;
 
 import static org.jcMetro.Player.O;
 import static org.jcMetro.Player.X;
 
 public class Othello {
+
+    private enum Direction{
+        North(-1, 0),
+        South(1,0);
+
+        public final int rowStep;
+        public final int colStep;
+
+        Direction(int rowStep, int colStep) {
+            this.rowStep = rowStep;
+            this.colStep = colStep;
+        }
+    }
+
+    public class Cell{
+
+        public final int rowIndex;
+        public final int colIndex;
+
+        public Cell(int rowIndex, int colIndex) {
+            this.rowIndex = rowIndex;
+            this.colIndex = colIndex;
+        }
+
+        public Cell moveTo(Direction direction) {
+            return new Cell(rowIndex + direction.rowStep, colIndex + direction.colStep);
+        }
+
+        @Override
+        public String toString() {
+            return "Cell{" +
+                    "rowIndex=" + rowIndex +
+                    ", colIndex=" + colIndex +
+                    '}';
+        }
+    }
+
 
     public static final int N = 8;
 
@@ -32,35 +68,36 @@ public class Othello {
 
         cells[rowIndex][colIndex] = player.symbol();
 
-        updateBoard();
-    }
+        // detect any flipping required
+        Cell currentCell = new Cell(rowIndex, colIndex);
+        Player currentPlayer = Player.valueOf(String.valueOf(cells[currentCell.rowIndex][currentCell.colIndex]));
+        Player oppositePlayer = currentPlayer == Player.O ? Player.X : Player.O;
 
-    private void updateBoard() {
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                if (cells[i][j] != '-' && needFlip(i,j)){
-                    flipIfNeeded(i,j);
-                }
+        Set<Cell> cellsToFlip = new HashSet<>();
+
+        for (Direction direction : Direction.values()) {
+            Cell searchCell = currentCell.moveTo(direction);
+            Set<Cell> potentialFlips = new HashSet<>();
+            boolean continueSearch = isWithinBoard(searchCell) && cells[searchCell.rowIndex][searchCell.colIndex] == oppositePlayer.symbol();
+
+            while(continueSearch){
+                potentialFlips.add(searchCell);
+                searchCell = searchCell.moveTo(direction);
+                continueSearch = isWithinBoard(searchCell) && cells[searchCell.rowIndex][searchCell.colIndex] == oppositePlayer.symbol();
             }
+
+            if (cells[searchCell.rowIndex][searchCell.colIndex] == currentPlayer.symbol()){
+                cellsToFlip.addAll(potentialFlips);
+            }
+        }
+
+        for (Cell cell : cellsToFlip) {
+            cells[cell.rowIndex][cell.colIndex] = currentPlayer.symbol();
         }
     }
 
-    private boolean needFlip(int row, int index) {
-        return false;
-    }
-
-    private void flipIfNeeded(int row, int col) {
-        char symbol = cells[row][col];
-        char opposite = symbol == 'X' ? 'O' : 'X';
-
-        Point northOpposite = null;
-        Point southOpposite = null;
-
-        for(int rowIndex = row; rowIndex >= 0; rowIndex--){
-            if (cells[rowIndex][col] == opposite){
-
-            }
-        }
+    private boolean isWithinBoard(Cell searchCell) {
+        return searchCell.rowIndex < N && searchCell.colIndex < N ;
     }
 
     public String displayBoard() {
