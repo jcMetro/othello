1b53ec99adc649dc7d72e34422f71e469efa779d
johnnychow <johnnykinchow@gmail.com>
2018-02-19 17:41:29 +0800

    full game flow, othello main method

--- src/test/java/org/jcMetro/CoordinateTest.java
+++ src/test/java/org/jcMetro/CoordinateTest.java
@@ -12,7 +12,7 @@ import static org.junit.Assert.*;
 public class CoordinateTest {
 
     @Test
-    public void parse_string_coordinate_into_3d() throws Exception {
+    public void parse_string_coordinate_into_3d() {
 
         List<Coordinate> coordinatesFor3d = Arrays.asList(new Coordinate("d3"), new Coordinate("3d"), new Coordinate("3D"), new Coordinate("D3"));
 
@@ -24,7 +24,7 @@ public class CoordinateTest {
     }
 
     @Test
-    public void parse_invalid_coordinates() throws Exception {
+    public void parse_invalid_coordinates() {
         List<Coordinate> invalidCoordinates = Arrays.asList(new Coordinate("a9"), new Coordinate("dd"), new Coordinate("88"), new Coordinate("HELLO"));
 
         for (Coordinate coordinate : invalidCoordinates) {
